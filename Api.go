d2d61fab25d544800749b20e3491150ffa1ae563
masterDeng <masterdeng@masterdeng-mb0.tencent.com>
2020-11-25 17:30:17 +0800

    can init struct

--- Api.go
+++ Api.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 /*Decode function
@@ -27,5 +28,39 @@ func Decode(data interface{}, s interface{}) error {
 	default:
 		fmt.Printf("unsupport type:%v\n", i)
 	}
+	SetStructFieldByJsonName(s, data.(map[string]interface{}))
 	return nil
+}
+
+/*SetStructFieldByJsonName init strcut by json tag, ref:https://www.cnblogs.com/fwdqxl/p/7789162.html
+* ptr the pointer of struct which want to be init
+* data use for init
+ */
+func SetStructFieldByJsonName(ptr interface{}, data map[string]interface{}) {
+
+	v := reflect.ValueOf(ptr).Elem() // the struct variable
+
+	for i := 0; i < v.NumField(); i++ {
+
+		fieldInfo := v.Type().Field(i) // a reflect.StructField
+		tag := fieldInfo.Tag           // a reflect.StructTag
+		name := tag.Get("json")
+
+		if name == "" {
+			name = strings.ToLower(fieldInfo.Name)
+		}
+		//去掉逗号后面内容 如 `json:"voucher_usage,omitempty"`
+		name = strings.Split(name, ",")[0]
+
+		if value, ok := data[name]; ok {
+			//给结构体赋值
+			//保证赋值时数据类型一致
+			if reflect.ValueOf(value).Type() == v.FieldByName(fieldInfo.Name).Type() {
+				v.FieldByName(fieldInfo.Name).Set(reflect.ValueOf(value))
+			}
+
+		}
+	}
+
+	return
 }
